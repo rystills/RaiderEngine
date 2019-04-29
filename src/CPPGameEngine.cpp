18edd70969ee4320a06bd77b2e3acef920ff419a
Ryan Stillings <rystills@gmail.com>
2019-04-29 05:38:15 -0400

    add gitignore, eclipse settings, simple rectangle render

---
+++ src/CPPGameEngine.cpp
@@ -0,0 +1,44 @@
+#include <GLFW/glfw3.h>
+
+int main(void) {
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window)) {
+		/* Poll for and process events */
+		glfwPollEvents();
+
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
+		glColor3f(1.0f, 0.0f, 0.0f); // Red
+		glVertex2f(-0.5f, -0.5f);    // x, y
+		glVertex2f( 0.5f, -0.5f);
+		glVertex2f( 0.5f,  0.5f);
+		glVertex2f(-0.5f,  0.5f);
+		glEnd();
+
+		glFlush();  // Render now
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
