d7afeb5d4279074453f6b75c24bfcbc911e5a375
Ryan Stillings <rystills@gmail.com>
2019-04-29 15:58:45 -0400

    add glad, remove glu32/glut32 from -l, glad helloWorld
    main code from the following tutorial: https://learnopengl.com/Getting-started/Hello-Window

--- src/CPPGameEngine.cpp
+++ src/CPPGameEngine.cpp
@@ -1,52 +1,89 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 #include <SOIL.h>
-#include <glm.hpp>
+#include <glm/glm.hpp>
 
-int main(void) {
-	GLFWwindow* window;
+#include <iostream>
 
-	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
 
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window) {
-		glfwTerminate();
-		return -1;
-	}
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
+int main() {
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window)) {
-		/* Poll for and process events */
-		glfwPollEvents();
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+#endif
 
-		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-		glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
-		glColor3f(1.0f, 0.0f, 0.0f); // Red
-		glVertex2f(-0.5f, -0.5f);    // x, y
-		glVertex2f( 0.5f, -0.5f);
-		glVertex2f( 0.5f,  0.5f);
-		glVertex2f(-0.5f,  0.5f);
-		glEnd();
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
 
-		glFlush();  // Render now
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window)) {
+        // input
+        // -----
+    	// glfw: poll IO events (keys pressed/released, mouse moved etc.)
+    	glfwPollEvents();
+        processInput(window);
 
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
-	}
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-	glfwTerminate();
-	return 0;
+        // glfw: swap buffers
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+    }
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
 }
