950614e87aeb6984b06241ccea4c9f51e61dee78
Ryan Stillings <rystills@gmail.com>
2019-05-21 00:21:38 -0400

    add normal map support to g_buffer shaders
    -normal map calculations adapted from LearnOpenGL Normal Mapping tutorial

--- CPPGameEngine/g_buffer.fs
+++ CPPGameEngine/g_buffer.fs
@@ -3,21 +3,28 @@ layout (location = 0) out vec3 gPosition;
 layout (location = 1) out vec3 gNormal;
 layout (location = 2) out vec4 gAlbedoSpec;
 
-in vec2 TexCoords;
-in vec3 FragPos;
-in vec3 Normal;
+in VS_OUT {
+    vec3 FragPos;
+    vec2 TexCoords;
+    mat3 TBN;
+} fs_in;
 
 uniform sampler2D texture_diffuse1;
 uniform sampler2D texture_specular1;
+uniform sampler2D texture_normal1;
 
 void main()
 {    
     // store the fragment position vector in the first gbuffer texture
-    gPosition = FragPos;
-    // also store the per-fragment normals into the gbuffer
-    gNormal = normalize(Normal);
+    gPosition = fs_in.FragPos;
+    
+	// also store the per-fragment normals into the gbuffer
+	// TODO: revert to standard normal calculation if no normal map is provided
+	// for correct normals, make sure your crazybump settings are configured to y-axis up, x-axis right
+    gNormal = normalize( 2.0 * texture(texture_normal1, fs_in.TexCoords).rgb - vec3(1.0)) * fs_in.TBN;
+	
     // and the diffuse per-fragment color
-    gAlbedoSpec.rgb = texture(texture_diffuse1, TexCoords).rgb;
+    gAlbedoSpec.rgb = texture(texture_diffuse1, fs_in.TexCoords).rgb;
     // store specular intensity in gAlbedoSpec's alpha component
-    gAlbedoSpec.a = texture(texture_specular1, TexCoords).r;
+    gAlbedoSpec.a = texture(texture_specular1, fs_in.TexCoords).r;
 }
--- CPPGameEngine/g_buffer.vs
+++ CPPGameEngine/g_buffer.vs
@@ -2,10 +2,14 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoords;
+layout (location = 3) in vec3 aTangent;
+layout (location = 4) in vec3 aBitangent;
 
-out vec3 FragPos;
-out vec2 TexCoords;
-out vec3 Normal;
+out VS_OUT {
+    vec3 FragPos;
+    vec2 TexCoords;
+    mat3 TBN;
+} vs_out;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -14,11 +18,16 @@ uniform mat4 projection;
 void main()
 {
     vec4 worldPos = model * vec4(aPos, 1.0);
-    FragPos = worldPos.xyz; 
-    TexCoords = aTexCoords;
+    vs_out.FragPos = worldPos.xyz; 
+    vs_out.TexCoords = aTexCoords;
     
     mat3 normalMatrix = transpose(inverse(mat3(model)));
-    Normal = normalMatrix * aNormal;
-
+    vec3 T = normalize(normalMatrix * aTangent);
+    vec3 N = normalize(normalMatrix * aNormal);
+    T = normalize(T - dot(T, N) * N);
+    vec3 B = cross(N, T);
+    
+    vs_out.TBN = transpose(mat3(T, B, N));    
+    
     gl_Position = projection * view * worldPos;
 }
