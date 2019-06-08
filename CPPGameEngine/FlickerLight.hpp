70b49c69af72d081041c4eea9fd592c8830482d9
Ryan Stillings <rystills@gmail.com>
2019-06-08 13:40:14 -0400

    Add randomness to FlickerLight toggle

--- CPPGameEngine/FlickerLight.hpp
+++ CPPGameEngine/FlickerLight.hpp
@@ -5,13 +5,29 @@
 
 class FlickerLight : public Light {
 public:
-	float elapsedTime = 0;
-	FlickerLight(glm::vec3 position, glm::vec3 color, float strength) :Light(position, color, strength) { }
-	
+	float elapsedTime = 0, prevTime = 0, flickerWait = chooseFlickerWait(3);
+	FlickerLight(glm::vec3 position, glm::vec3 color, float strength) :Light(position, color, strength) {
+	}
+
+	/*
+	choose how long the light should wait before switching states again
+	@param maxSeconds: the maximum time (in seconds) the light can wait
+	@returns: the randomly chosen time (in seconds) the light should wait before switching states
+	*/
+	float chooseFlickerWait(float maxSeconds) {
+		return (rand()) / static_cast <float> (RAND_MAX/maxSeconds);
+	}
+
 	void update(float deltaTime) override {
 		elapsedTime += deltaTime;
 		Light::update(deltaTime);
-		on = (((int)elapsedTime) % 2);
+		// flicker once flickerWait time has elapsed
+		if (prevTime + flickerWait < elapsedTime) {
+			on = !on;
+			prevTime += flickerWait;
+			// choose a new time to wait within a small range when the light is off, and a much larger range when the light is on
+			flickerWait = chooseFlickerWait(on ? 3 : .45f);
+		}
 	}
 };
 
