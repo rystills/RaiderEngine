305995c3001c4375dbd3836ccfad9c4e6f589b9d
Ryan Stillings <rystills@gmail.com>
2020-03-12 03:42:47 -0400

    shadows: remove manual PCF, implement Poisson Sampling, early bail test

--- RaiderEngine/shaders/deferred_shading.fs
+++ RaiderEngine/shaders/deferred_shading.fs
@@ -30,15 +30,32 @@ uniform int NR_LIGHTS;
 uniform Light lights[5]; // hard-coded maximum of 5 lights for now
 uniform vec3 viewPos;
 
-// array of offset direction for sampling
-#define numSamples 20
-vec3 gridSamplingDisk[numSamples] = vec3[] (
-   vec3(1, 1,  1), vec3( 1, -1,  1), vec3(-1, -1,  1), vec3(-1, 1,  1), 
-   vec3(1, 1, -1), vec3( 1, -1, -1), vec3(-1, -1, -1), vec3(-1, 1, -1),
-   vec3(1, 1,  0), vec3( 1, -1,  0), vec3(-1, -1,  0), vec3(-1, 1,  0),
-   vec3(1, 0,  1), vec3(-1,  0,  1), vec3( 1,  0, -1), vec3(-1, 0, -1),
-   vec3(0, 1,  1), vec3( 0, -1,  1), vec3( 0, -1, -1), vec3( 0, 1, -1)
+#define numPoisson 16
+vec2 poissonDisk[numPoisson] = vec2[]( 
+   vec2( -0.94201624, -0.39906216 ), 
+   vec2( 0.94558609, -0.76890725 ), 
+   vec2( -0.094184101, -0.92938870 ), 
+   vec2( 0.34495938, 0.29387760 ), 
+   vec2( -0.91588581, 0.45771432 ), 
+   vec2( -0.81544232, -0.87912464 ), 
+   vec2( -0.38277543, 0.27676845 ), 
+   vec2( 0.97484398, 0.75648379 ), 
+   vec2( 0.44323325, -0.97511554 ), 
+   vec2( 0.53742981, -0.47373420 ), 
+   vec2( -0.26496911, -0.41893023 ), 
+   vec2( 0.79197514, 0.19090188 ), 
+   vec2( -0.24188840, 0.99706507 ), 
+   vec2( -0.81409955, 0.91437590 ), 
+   vec2( 0.19984126, 0.78641367 ), 
+   vec2( 0.14383161, -0.14100790 ) 
 );
+
+// Returns a random number based on a vec3 and an int.
+float random(vec3 seed, int i){
+	vec4 seed4 = vec4(seed,i);
+	float dot_product = dot(seed4, vec4(12.9898,78.233,45.164,94.673));
+	return fract(sin(dot_product) * 43758.5453);
+}
 
 float ShadowCalculation(vec3 fragPos, vec3 normal, vec3 lightPos, int lightNum) {
     // get vector between fragment position and light position
@@ -46,7 +63,7 @@ float ShadowCalculation(vec3 fragPos, vec3 normal, vec3 lightPos, int lightNum) 
     // now get current linear depth as the length between the fragment and light position
     float currentDepth = length(fragToLight);
     // test for shadows
-    float shadow = 0.0;
+    int shadow = 0;
 	// note: tweak the bias as you see fit. Higher bias will cause less shadow acne but more shadow displacement
 	vec3 lightDir = normalize(lightPos - fragPos);
     // NOTE: decreasing the bias range results in less peter panning but more shadow acne (tweaked from .05-.005 to .15-.03)
@@ -54,19 +71,22 @@ float ShadowCalculation(vec3 fragPos, vec3 normal, vec3 lightPos, int lightNum) 
     float viewDistance = length(viewPos - fragPos);
     // NOTE: increasing the divisor results in harder shadows (tweaked from 25 to 50)
     float diskRadius = (1.0 + (viewDistance / far_plane)) / 50.0;
-    for(int i = 0; i < numSamples; ++i) {
+    // TODO: reimplement PCF on hardware (sampler2DShadow / samplerCubeShadow)
+    for (int i = 0; i < 20; ++i) { 
+        int index = int(numPoisson*random(floor(fragPos*1000.0), i))%numPoisson;
         // use the fragment to light vector to sample from the depth map
-        float closestDepth = texture(lightNum == 0 ? depthMap0 : (lightNum == 1 ? depthMap1 : (lightNum == 2 ? depthMap2 : (lightNum == 3 ? depthMap3 : depthMap4))), fragToLight + gridSamplingDisk[i] * diskRadius).r;
+        // TODO: 3d poisson sample rather than using the x value of the next entry for the 3rd dimension
+        float closestDepth = texture(lightNum == 0 ? depthMap0 : (lightNum == 1 ? depthMap1 : (lightNum == 2 ? depthMap2 : (lightNum == 3 ? depthMap3 : depthMap4))), fragToLight + vec3(poissonDisk[index],poissonDisk[(index+1)%numPoisson].x) * diskRadius).r;
         closestDepth *= far_plane;   // undo mapping [0;1]
-        if(currentDepth - bias > closestDepth)
-            shadow += 1.0;
+        shadow += int(currentDepth - bias > closestDepth);
+        // NOTE: early bail test (bailing after more iterations improves edge detection but reduces performance)
+        if (i == 3 && (shadow == 0 || shadow == 4))
+            return shadow/4.0;
     }
-    shadow /= float(numSamples);
+    return shadow / float(20);
         
     // display closestDepth as debug (to visualize depth cubemap)
     // FragColor = vec4(vec3(closestDepth / far_plane), 1.0);    
-        
-    return shadow;
 }
 
 void main()
